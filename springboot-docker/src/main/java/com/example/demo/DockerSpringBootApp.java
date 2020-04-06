0706f05c8e4553797c790a54783e31455d147a89
apps2-osce <51912639+apps2-osce@users.noreply.github.com>
2020-04-06 21:03:18 +0530

    springboot-docker

---
+++ springboot-docker/src/main/java/com/example/demo/DockerSpringBootApp.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootApp.class, args);
+	}
+
+}
